be6c3173513a26e33e28bbc5d546e225aeb99230
sungwoong-Noh <sw.noh2@okestro.com>
2023-12-05 16:29:12 +0900

    feat: 커밋 테스트

--- src/main/java/com/example/assignment/article/svc/ArticleService.java
+++ src/main/java/com/example/assignment/article/svc/ArticleService.java
@@ -3,18 +3,22 @@ package com.example.assignment.article.svc;
 import com.example.assignment.article.config.ArticleList;
 import com.example.assignment.article.domain.Article;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleService {
 
     private final ArticleList articleList;
 
 
     public List<Article> getArticleList() {
+
+        log.info("==== 게시글 조회 ====");
 
         return articleList.getDataList();
     }
